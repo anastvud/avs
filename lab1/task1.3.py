1baab814d5b02ecabb73314c6aafdf300ed6de04
anastvud <anastasiiavudvud@gmail.com>
2024-03-07 14:15:24 +0100

    lab1 v.f

--- lab1/task1.3.py
+++ lab1/task1.3.py
@@ -5,7 +5,12 @@ i = cv2.imread("/home/nastia/agh/avs/lab1/mandrill.jpg")
 IG = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
 IHSV = cv2.cvtColor(i, cv2.COLOR_BGR2HSV)
 
+H, S, V = IHSV[:,:,0], IHSV[:,:,1], IHSV[:,:,2]
+
 cv2.imshow("IG", IG)
 cv2.imshow("IHSV", IHSV)
+cv2.imshow("H", H)
+cv2.imshow("S", S)
+cv2.imshow("V", V)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- lab1/task1.5.py
+++ lab1/task1.5.py
@@ -8,9 +8,21 @@ lena = cv2.imread("/home/nastia/agh/avs/lab1/lena.png")
 
 m_gray = cv2.cvtColor(madrill, cv2.COLOR_BGR2GRAY)
 l_gray = cv2.cvtColor(lena, cv2.COLOR_BGR2GRAY)
-
-# resize m_gray to add two pictures
 m_gray = cv2.resize(m_gray, (l_gray.shape[1], l_gray.shape[0]))
+
+plt.figure(figsize=(18, 10))
+
+plt.subplot(2, 4, 1)
+plt.title("m_gray")
+plt.imshow(cv2.cvtColor(m_gray, cv2.COLOR_BGR2RGB))
+
+plt.subplot(2, 4, 2)
+plt.title("l_gray")
+plt.imshow(cv2.cvtColor(l_gray, cv2.COLOR_BGR2RGB))
+
+# change to int32 to add two pictures
+m_gray = m_gray.astype('int')
+l_gray = l_gray.astype('int')
 
 sum_gray = np.add(m_gray, l_gray)
 diff_gray = np.subtract(m_gray, l_gray)
@@ -24,35 +36,28 @@ lin_comb = cv2.addWeighted(m_gray, alpha, l_gray, beta, 0.0)
 # absdiff
 mod_diff = cv2.absdiff(m_gray, l_gray)
 
-
-plt.figure(figsize=(18, 10))
-
-plt.subplot(2, 4, 1)
-plt.title("m_gray")
-plt.imshow(cv2.cvtColor(m_gray, cv2.COLOR_BGR2RGB))
-
-plt.subplot(2, 4, 2)
-plt.title("l_gray")
-plt.imshow(cv2.cvtColor(l_gray, cv2.COLOR_BGR2RGB))
+info = np.iinfo(m_gray.dtype)
+m_gray = ((m_gray / info.max) * 255).astype(np.uint8)
+l_gray = ((l_gray / info.max) * 255).astype(np.uint8)
 
 plt.subplot(2, 4, 3)
 plt.title("sum_gray")
-plt.imshow(cv2.cvtColor(sum_gray, cv2.COLOR_BGR2RGB))
+plt.imshow(sum_gray, cmap='gray')
 
 plt.subplot(2, 4, 4)
 plt.title("diff_gray")
-plt.imshow(cv2.cvtColor(diff_gray, cv2.COLOR_BGR2RGB))
+plt.imshow(diff_gray, cmap='gray')
 
 plt.subplot(2, 4, 5)
 plt.title("mult_gray")
-plt.imshow(cv2.cvtColor(mult_gray, cv2.COLOR_BGR2RGB))
+plt.imshow(mult_gray, cmap='gray')
 
 plt.subplot(2, 4, 6)
 plt.title("lin_comb")
-plt.imshow(cv2.cvtColor(lin_comb, cv2.COLOR_BGR2RGB))
+plt.imshow(lin_comb, cmap='gray')
 
 plt.subplot(2, 4, 7)
 plt.title("mod_diff")
-plt.imshow(cv2.cvtColor(mod_diff, cv2.COLOR_BGR2RGB))
+plt.imshow(mod_diff, cmap='gray')
 
 plt.show()
