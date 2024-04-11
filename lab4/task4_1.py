fef00922c4959a8a69483820e33cb8049f39c833
anastvud <anastasiiavudvud@gmail.com>
2024-04-11 16:20:21 +0200

    final ver

--- lab4/task4_1.py
+++ lab4/task4_1.py
@@ -2,12 +2,10 @@ import cv2
 import numpy as np
 
 
-def of(I_org, I, J, W2=3, dY=3, dX=3):
+def of(I, J, W2=3, dY=3, dX=3):
 
     u = np.zeros((I.shape[0], I.shape[1]), dtype=np.float32)
     v = np.zeros((I.shape[0], I.shape[1]), dtype=np.float32)
-
-    mod_diff = cv2.absdiff(I.astype("int"), J.astype("int")).astype(np.uint8)
 
     for j in range(W2, I.shape[0] - W2):
         for i in range(W2, I.shape[1] - W2):
@@ -48,8 +46,8 @@ def vis_flow(u, v, YX, name):
 
     optical_flow_hsv = np.zeros((YX[0], YX[1], 3), dtype=np.uint8)
     optical_flow_hsv[..., 0] = angle
-    optical_flow_hsv[..., 1] = 255
-    optical_flow_hsv[..., 2] = magnitude
+    optical_flow_hsv[..., 1] = magnitude
+    optical_flow_hsv[..., 2] = 255
     optical_flow_bgr = cv2.cvtColor(optical_flow_hsv, cv2.COLOR_HSV2BGR)
 
     cv2.namedWindow(name, cv2.WINDOW_NORMAL)
@@ -58,12 +56,13 @@ def vis_flow(u, v, YX, name):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+if (__name__ == "__main__"):
 
-I = cv2.imread("/home/nastia/agh/avs/lab4/lab_files/I.jpg")
-J = cv2.imread("/home/nastia/agh/avs/lab4/lab_files/J.jpg")
+    I = cv2.imread("/home/nastia/agh/avs/lab4/lab_files/I.jpg")
+    J = cv2.imread("/home/nastia/agh/avs/lab4/lab_files/J.jpg")
 
-I = cv2.cvtColor(I, cv2.COLOR_RGB2GRAY)
-J = cv2.cvtColor(J, cv2.COLOR_RGB2GRAY)
+    I = cv2.cvtColor(I, cv2.COLOR_RGB2GRAY)
+    J = cv2.cvtColor(J, cv2.COLOR_RGB2GRAY)
 
-u, v = of(I, I, J)
-vis_flow(u, v, I.shape, "Optical Flow")
+    u, v = of(I, I, J)
+    vis_flow(u, v, I.shape, "Optical Flow")
--- lab4/task4_2.py
+++ lab4/task4_2.py
@@ -9,30 +9,44 @@ def pyramid(im, max_scale):
         images.append(cv2.resize(images[k - 1], (0, 0), fx=0.5, fy=0.5))
     return images
 
+
 def optical_flow_multiscale(I_org, J, max_scale=3, W2=3, dY=3, dX=3):
     IP = pyramid(I_org, max_scale)
-    I = IP[-1] 
+    JP = pyramid(J, max_scale)
+
+    u_or, v_or = of(I_org, J)
+
+    u_total = np.zeros_like(u_or)
+    v_total = np.zeros_like(v_or)
+
+    I = IP[-1]
 
     for scale in range(max_scale - 1, -1, -1):
-        u_scale, v_scale = of(IP[scale], I, J, W2, dY, dX)
+        J = JP[scale]
+
+        u, v = of(I, J, W2, dY, dX)
+        # vis_flow(u, v, I.shape, "Optical Flow")
         
+        
+        I_new = np.copy(I)
         if scale > 0:
-            I_new = cv2.resize(I, (0, 0), fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
             for j in range(I_new.shape[0]):
                 for i in range(I_new.shape[1]):
-                    if 0 <= j - v_scale[j, i] * 2 < I.shape[0] and 0 <= i - u_scale[j, i] * 2 < I.shape[1]:
-                        I_new[j, i] = I[j - v_scale[j, i] * 2, i - u_scale[j, i] * 2]
-            I = I_new
+                    if 0 <= j + v[j,i] < I_new.shape[0] and 0 <= i + u[j, i] < I_new.shape[1]:
+                        I_new[(j + v[j,i]).astype('int'), (i + u[j, i]).astype('int')] = I[j, i]
 
-    u_total = np.zeros_like(u_scale)
-    v_total = np.zeros_like(v_scale)
+            I = cv2.resize(I_new, (0, 0), fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
 
-    for scale in range(max_scale):
-        u_scale, v_scale = of(IP[scale], I_org, J, W2, dY, dX)
-        u_total += u_scale
-        v_total += v_scale
 
-    vis_flow(u_total, v_total, I_org.shape, "Optical Flow Multiscale")
+
+        u_total += cv2.resize(u * (2 ** scale), (0, 0), fx=2 ** scale, fy=2 ** scale, interpolation=cv2.INTER_LINEAR)
+        v_total += cv2.resize(v * (2 ** scale), (0, 0), fx=2 ** scale, fy=2 ** scale, interpolation=cv2.INTER_LINEAR)
+            
+
+
+    vis_flow(u_total, v_total, I_org.shape, "Optical Flow")
+
+
 
 I = cv2.imread("/home/nastia/agh/avs/lab4/lab_files/I.jpg")
 J = cv2.imread("/home/nastia/agh/avs/lab4/lab_files/J.jpg")
