ca3415f71ddebc155bf1f3245e5465363222a480
anastvud <anastasiiavudvud@gmail.com>
2024-04-24 13:08:10 +0200

    disparsity map for uncalibrated imgs

--- lab5/algorithm_template_for_calibration/task5_3.py
+++ lab5/algorithm_template_for_calibration/task5_3.py
@@ -10,39 +10,34 @@ img_width = 640
 img_height = 480
 
 image_dir = os.getenv("REPO_ROOT") + "/lab5/dataset/example/"
-number_of_images = 1
-
-calibration_img = cv2.imread(os.getenv("REPO_ROOT") + "/lab5/dataset/pairs/left_01.png")
-
 img = cv2.imread(image_dir + "example0.jpg")
 N, XX, YY = img.shape[::-1]
 left_img = np.zeros((YY, int(XX / 2), N), np.uint8)
 right_img = np.copy(left_img)
-
 left_img = img[:, 0 : int(XX / 2) :, :]
 right_img = img[:, int(XX / 2) : XX :, :]
 
-left_img = cv2.resize(
-    left_img, (int(calibration_img.shape[1]), int(calibration_img.shape[0]))
-)
-right_img = cv2.resize(
-    right_img, (int(calibration_img.shape[1]), int(calibration_img.shape[0]))
-)
-
-left_img_uncalibrated = cv2.cvtColor(left_img, cv2.COLOR_BGR2RGB)
-right_img_uncalibrated = cv2.cvtColor(right_img, cv2.COLOR_BGR2RGB)
-
+left_img = cv2.resize(left_img, (img_width, img_height))
+right_img = cv2.resize(right_img, (img_width, img_height))
 
 left_rectified = cv2.remap(left_img, map1_left, map2_left, cv2.INTER_LINEAR)
 right_rectified = cv2.remap(right_img, map1_right, map2_right, cv2.INTER_LINEAR)
 
-left_gray = cv2.cvtColor(left_rectified, cv2.COLOR_BGR2GRAY)
-right_gray = cv2.cvtColor(right_rectified, cv2.COLOR_BGR2GRAY)
+left_rect_gray = cv2.cvtColor(left_rectified, cv2.COLOR_BGR2GRAY)
+right_rect_gray = cv2.cvtColor(right_rectified, cv2.COLOR_BGR2GRAY)
+left_gray = cv2.cvtColor(left_img, cv2.COLOR_BGR2GRAY)
+right_gray = cv2.cvtColor(right_img, cv2.COLOR_BGR2GRAY)
 
 block_matcher = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-disparity_bm = block_matcher.compute(left_gray, right_gray)
 sgm_matcher = cv2.StereoSGBM_create(minDisparity=0, numDisparities=16, blockSize=15)
-disparity_sgm = sgm_matcher.compute(left_gray, right_gray)
+
+disparity_bm = block_matcher.compute(left_rect_gray, right_rect_gray)
+disparity_sgm = sgm_matcher.compute(left_rect_gray, right_rect_gray)
+disparity_bm_unrec = block_matcher.compute(left_gray, right_gray)
+disparity_sgm_unrec = sgm_matcher.compute(left_gray, right_gray)
+
+
+
 disparity_bm_norm = cv2.normalize(
     disparity_bm, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U
 )
@@ -50,34 +45,51 @@ disparity_sgm_norm = cv2.normalize(
     disparity_sgm, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U
 )
 
-heatmap_bm = cv2.applyColorMap(disparity_bm_norm, cv2.COLORMAP_MAGMA)
-heatmap_sgm = cv2.applyColorMap(disparity_sgm_norm, cv2.COLORMAP_MAGMA)
+disparity_bm_unrec_norm = cv2.normalize(
+    disparity_bm_unrec, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U
+)
+disparity_sgm_unrec_norm = cv2.normalize(
+    disparity_sgm_unrec, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U
+)
 
+heatmap_bm = cv2.applyColorMap(disparity_bm_norm, cv2.COLORMAP_HOT)
+heatmap_sgm = cv2.applyColorMap(disparity_sgm_norm, cv2.COLORMAP_HOT)
+
+heatmap_bm_unrec = cv2.applyColorMap(disparity_bm_unrec_norm, cv2.COLORMAP_HOT)
+heatmap_sgm_unrec = cv2.applyColorMap(disparity_sgm_unrec_norm, cv2.COLORMAP_HOT)
 
 plt.figure(figsize=(18, 10))
 
-plt.subplot(3, 2, 1)
+plt.subplot(4, 2, 1)
 plt.title("left_img")
 plt.imshow(left_img)
 
-plt.subplot(3, 2, 2)
+plt.subplot(4, 2, 2)
 plt.title("right_img")
 plt.imshow(right_img)
 
-plt.subplot(3, 2, 3)
+plt.subplot(4, 2, 3)
 plt.title("left_rectified")
 plt.imshow(left_rectified)
 
-plt.subplot(3, 2, 4)
+plt.subplot(4, 2, 4)
 plt.title("right_rectified")
 plt.imshow(right_rectified)
 
-plt.subplot(3, 2, 5)
+plt.subplot(4, 2, 5)
 plt.title("heatmap_bm")
 plt.imshow(heatmap_bm)
 
-plt.subplot(3, 2, 6)
+plt.subplot(4, 2, 6)
 plt.title("heatmap_sgm")
 plt.imshow(heatmap_sgm)
+
+plt.subplot(4, 2, 7)
+plt.title("heatmap_bm_unrec")
+plt.imshow(heatmap_bm_unrec)
+
+plt.subplot(4, 2, 8)
+plt.title("heatmap_sgm_unrec")
+plt.imshow(heatmap_sgm_unrec)
 
 plt.show()
